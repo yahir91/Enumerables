8ba3d12ed4dfaa74409ce1f415c28393b25acbb4
MarilenaRoque <marilenas@id.uff.br>
2020-06-15 14:12:46 -0300

    Built new method and Validators File
    Added all the validators and formating files
    Built the my_each_with_index

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org' do
+  gem 'rubocop', '~>0.81.0'
+end
--- ruby.rb
+++ ruby.rb
@@ -1,15 +1,21 @@
-# frozen_string_literal: true
-
 module Enumerable
-  def my_each ()
+  def my_each
     i = 0
-    while i < self.length
+    while i < length
       yield(self[i])
+      i += 1
+    end
+  end
+
+  def my_each_with_index
+    i = 0
+    while i < length
+      yield(self[i], i)
       i += 1
     end
   end
 end
 
-[1, 3, 4, 5, 5, 9].my_each do |x|
-  puts x
+[1, 3, 4, 5, 5, 9].my_each_with_index do |x, y|
+  puts x, y
 end
