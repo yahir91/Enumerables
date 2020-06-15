3290fa67751d03c29b744172c34a1d0af1397980
MarilenaRoque <marilenas@id.uff.br>
2020-06-15 15:04:13 -0300

    Built my_all? method

--- ruby.rb
+++ ruby.rb
@@ -23,8 +23,23 @@ module Enumerable
     end
     puts array
   end
+
+  def my_all?
+    something_false = 0
+    my_each do |x| 
+      if yield(x) == false || yield(x) == nil
+        something_false = 1
+      end
+    end
+    if something_false == 0
+      true
+    else 
+      false
+    end
+  end
+
 end
 
-[1, 3, 4, 5, 5, 9].my_select do |n|
-  n == 5
-end
+p([5, 5, 5, 5, 5, 5].my_all? do |x|
+  x==5
+end)
